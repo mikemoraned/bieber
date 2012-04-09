ff4d37eee7fd95bd6d0e2e6a72980dc2da81afb5
mikemoraned <mike@houseofmoran.com>
2012-04-09 18:27:57 +0100

    simplified

--- public/js/bieber.js
+++ public/js/bieber.js
@@ -7,31 +7,27 @@ function BieberAppModel() {
     this.bieberAge = Math.floor((this.currentTime - this.bieberBornAt) / this.MILLIS_PER_YEAR);
 
     this.userAge = ko.observable();
-
-    this.haveBigBieberAge = ko.observable(false);
-    this.userAgeInBigBiebers = ko.computed(function() {
+    this.haveValidUserAge = ko.observable(false);
+    this.validUserAge = ko.computed(function() {
 	var validAge = parseFloat(this.userAge());
 	if (validAge) {
-	    this.haveBigBieberAge(true);
-	    return Math.floor(validAge / this.bieberAge);
+	    this.haveValidUserAge(true);
+	    return validAge;
 	} else {
-	    this.haveBigBieberAge(false);
+	    this.haveValidUserAge(false);
 	}
+    }, this)
+
+    this.userAgeInBigBiebers = ko.computed(function() {
+	return Math.floor(this.validUserAge() / this.bieberAge);
     }, this);
 
     this.bieberRate = ko.observable(3);
-    this.haveMiniBieberAge = ko.observable(false);
     this.userAgeInMiniBiebers = ko.computed(function() {
-	var validAge = parseFloat(this.userAge());
-	if (validAge) {
-	    this.haveMiniBieberAge(true);
-	    return Math.floor((validAge * this.SECONDS_PER_YEAR) / this.bieberRate());
-	} else {
-	    this.haveMiniBieberAge(false);
-	}
+	return Math.floor((this.validUserAge() * this.SECONDS_PER_YEAR) / this.bieberRate());
     }, this);
-
 }
 
 var model = new BieberAppModel();
 ko.applyBindings(model);
+haveValidUserAge
