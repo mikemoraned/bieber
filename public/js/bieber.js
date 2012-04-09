e8e694507d8a87e8e4ebf693245c0df41a3aa873
mikemoraned <mike@houseofmoran.com>
2012-04-09 20:50:49 +0100

    added proper units

--- public/js/bieber.js
+++ public/js/bieber.js
@@ -1,3 +1,25 @@
+function withUnits(num) {
+    var unitForPower = {
+	0: { name: '',      divisor: 1 },
+	1: { name: 'deca',  divisor: 10 },
+	2: { name: 'hecto', divisor: 100 },
+	3: { name: 'kilo',  divisor: 1000 },
+	6: { name: 'mega',  divisor: 1000000 },
+	9: { name: 'giga',  divisor: 1000000000 }
+    };
+    if (num) {
+	var power = Math.floor(Math.log(num) / Math.log(10));
+	while (power > 0 && !unitForPower[power]) {
+	    power = power - 1;
+	}
+	var unit = unitForPower[power];
+	return { value: num / unit.divisor, unit: unit.name };
+    }
+    else {
+	return { value: num, unit: '' };
+    }
+}
+
 function BieberAppModel() {
     this.SECONDS_PER_YEAR = (365 * 24 * 60 * 60);
     this.MILLIS_PER_YEAR = (this.SECONDS_PER_YEAR * 1000.0);
@@ -22,12 +44,12 @@ function BieberAppModel() {
 	return Math.floor(this.validUserAge() / this.bieberAge);
     }, this);
 
-    this.bieberRate = ko.observable(3);
+    this.bieberRate = ko.observable(30);
     this.userAgeInMiniBiebers = ko.computed(function() {
-	return Math.floor((this.validUserAge() * this.SECONDS_PER_YEAR) / this.bieberRate());
+	var biebers = Math.floor((this.validUserAge() * this.SECONDS_PER_YEAR) / this.bieberRate());
+	return withUnits(biebers);
     }, this);
 }
 
 var model = new BieberAppModel();
 ko.applyBindings(model);
-haveValidUserAge
